11f8dd2d14eca606e6d7058e0f4acd946ce88963
loganmcdavid11 <loganmcdavid21@gmail.com>
2024-09-25 11:33:12 -0500

    Init commit scrimmage,py

---
+++ scrimmage.py
@@ -0,0 +1,16 @@
+"""
+Name: Logan McDavid
+Filename: scrimmage.py    
+Purpose: Contains scrimamge class for
+scrimmage_commands
+"""
+
+class Scrimmage:
+    def __init__(self):
+        self.purple_team = []
+        self.gold_team = []
+        self.signed_up_players = []
+        self.cutters = []
+        self.handlers = []
+        self.hybrids = []
+        
